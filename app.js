e57b8488e5038cd22d625271697aa4590d2a7c94
Debashis Naskar <56424991+debanaskar13@users.noreply.github.com>
2020-09-21 21:12:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const wallpapers = ['aisvri-lDZNNvLU6pY-unsplash.jpg','andreea-maria-juganaru-5s3QCQONRLc-unsplash.jpg','kim-giseok-x1pvYTFmbdI-unsplash.jpg','robinson-recalde-UjISjvPFksI-unsplash.jpg'];
 let counter = 1;
 
-/*document.querySelector('#next').addEventListener('click',function(){
+document.querySelector('#next').addEventListener('click',function(){
     counter=counter%wallpapers.length;
     document.querySelector('#picture').src = `images/${wallpapers[counter]}`;
     counter++;
-})*/
+})
 
-setInterval(function(){
+/*setInterval(function(){
     counter=counter%wallpapers.length;
     document.querySelector('#picture').src = `images/${wallpapers[counter]}`;
     counter++;
-},2000)
+},2000)*/
